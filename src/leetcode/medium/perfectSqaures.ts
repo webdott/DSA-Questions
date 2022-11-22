dfa370b3a424b1cbd0a28a203138065db336d74d
caspero-62 <nwaforuchechukwu2007@gmail.com>
2022-11-23 00:46:42 +0100

    more solutions to questions

--- src/leetcode/medium/perfectSqaures.ts
+++ src/leetcode/medium/perfectSqaures.ts
@@ -62,3 +62,22 @@ const numSquares = (n: number): number => {
 
 	return -1;
 };
+
+/**
+ *
+ * @param n number
+ * @returns number
+ */
+const numSquaresDP = (n: number): number => {
+	const dp: number[] = new Array(n + 1).fill(Infinity);
+	dp[0] = 0;
+	dp[1] = 1;
+
+	for (let i = 2; i <= n; i++) {
+		for (let j = 1; j * j <= i; j++) {
+			dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+		}
+	}
+
+	return dp[n];
+};
