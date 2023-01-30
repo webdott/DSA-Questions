4e26ace172db8e8d2fddab8d1a0eb17c85b981ef
caspero-62 <nwaforuchechukwu2007@gmail.com>
2023-01-30 22:29:34 +0100

    more solutions to questions

---
+++ src/leetcode/easy/nthTribonacciNumber.ts
@@ -0,0 +1,44 @@
+/**
+    * desc: The Tribonacci sequence Tn is defined as follows: 
+
+    T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+    Given n, return the value of Tn.
+
+    Example 1: =>
+    Input: n = 4
+    Output: 4
+    Explanation:
+    T_3 = 0 + 1 + 1 = 2
+    T_4 = 1 + 1 + 2 = 4
+    
+    Example 2: =>
+    Input: n = 25
+    Output: 1389537
+    
+    Constraints: =>
+    0 <= n <= 37
+    The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+ */
+
+const cache = {};
+
+/**
+ *
+ * @param n number
+ * @returns number
+ */
+const tribonacci = (n: number): number => {
+	if (cache[n]) return cache[n];
+
+	if (n === 0) {
+		return 0;
+	} else if (n === 1) {
+		return 1;
+	} else if (n === 2) {
+		return 1;
+	} else {
+		return (cache[n] =
+			tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3));
+	}
+};
