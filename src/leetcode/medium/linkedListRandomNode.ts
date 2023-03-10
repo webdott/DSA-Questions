2e3fee216fac7fb439bdd1704d1c17e108427bac
caspero-62 <nwaforuchechukwu2007@gmail.com>
2023-03-10 11:17:16 +0100

    more solutions to questions

---
+++ src/leetcode/medium/linkedListRandomNode.ts
@@ -0,0 +1,80 @@
+/**
+    * desc: Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+    Implement the Solution class:
+
+    Solution(ListNode head) Initializes the object with the head of the singly-linked list head.
+    int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.
+
+    Example 1: =>
+    Input
+    ["Solution", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom"]
+    [[[1, 2, 3]], [], [], [], [], []]
+    Output
+    [null, 1, 3, 2, 2, 3]
+
+    Explanation
+    Solution solution = new Solution([1, 2, 3]);
+    solution.getRandom(); // return 1
+    solution.getRandom(); // return 3
+    solution.getRandom(); // return 2
+    solution.getRandom(); // return 2
+    solution.getRandom(); // return 3
+    // getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+    
+    Constraints: =>
+    The number of nodes in the linked list will be in the range [1, 104].
+    -104 <= Node.val <= 104
+    At most 104 calls will be made to getRandom.
+    
+    Follow up: =>
+    What if the linked list is extremely large and its length is unknown to you?
+    Could you solve this efficiently without using extra space?
+ */
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class ListNode {
+	val: number;
+	next: ListNode | null;
+	constructor(val?: number, next?: ListNode | null) {
+		this.val = val === undefined ? 0 : val;
+		this.next = next === undefined ? null : next;
+	}
+}
+
+class Solution {
+	private roulette: number[];
+
+	constructor(head: ListNode) {
+		this.roulette = [];
+		let currIter: ListNode = head;
+
+		while (currIter) {
+			this.roulette.push(currIter.val);
+			currIter = currIter.next!;
+		}
+	}
+
+	getRandom(): number {
+		const luckyNumber = ~~(Math.random() * this.roulette.length);
+
+		return this.roulette[luckyNumber] ?? -1;
+	}
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(head)
+ * var param_1 = obj.getRandom()
+ */
