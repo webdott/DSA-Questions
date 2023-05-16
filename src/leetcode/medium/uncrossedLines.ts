36531ea22b2be2c0691dc19b417b07723317d353
caspero-62 <nwaforuchechukwu2007@gmail.com>
2023-05-16 16:41:00 +0100

    more solutions to questions

---
+++ src/leetcode/medium/uncrossedLines.ts
@@ -0,0 +1,62 @@
+/**
+    * desc: You are given two integer arrays nums1 and nums2. We write the integers of nums1 and nums2 (in the order they are given) on two separate horizontal lines.
+
+    We may draw connecting lines: a straight line connecting two numbers nums1[i] and nums2[j] such that:
+
+    nums1[i] == nums2[j], and
+    the line we draw does not intersect any other connecting (non-horizontal) line.
+    Note that a connecting line cannot intersect even at the endpoints (i.e., each number can only belong to one connecting line).
+
+    Return the maximum number of connecting lines we can draw in this way.
+
+    Example 1: =>
+    Input: nums1 = [1,4,2], nums2 = [1,2,4]
+    Output: 2
+    Explanation: We can draw 2 uncrossed lines as in the diagram.
+    We cannot draw 3 uncrossed lines, because the line from nums1[1] = 4 to nums2[2] = 4 will intersect the line from nums1[2]=2 to nums2[1]=2.
+    
+    Example 2: =>
+    Input: nums1 = [2,5,1,2,5], nums2 = [10,5,2,1,5,2]
+    Output: 3
+    
+    Example 3: =>
+    Input: nums1 = [1,3,7,1,7,5], nums2 = [1,9,2,5,1]
+    Output: 2
+    
+    Constraints: =>
+    1 <= nums1.length, nums2.length <= 500
+    1 <= nums1[i], nums2[j] <= 2000
+ */
+
+/**
+ *
+ * @param nums1 number[]
+ * @param nums2 number[]
+ * @returns number
+ */
+const maxUncrossedLines = (nums1: number[], nums2: number[]): number => {
+	const memo: Record<string, number> = {};
+
+	function recurse(i: number, j: number) {
+		if (i >= nums1.length || j >= nums2.length) return 0;
+
+		let key: string = `${i}-${j}`;
+
+		if (memo[key] !== undefined) return memo[key];
+
+		let maxNum: number = 0;
+
+		if (nums1[i] === nums2[j]) {
+			// once we encounter two same numbers
+			maxNum += 1;
+			maxNum += recurse(i + 1, j + 1);
+		} else {
+			// once we encounter two different numbers
+			maxNum = Math.max(recurse(i + 1, j), recurse(i, j + 1));
+		}
+
+		return (memo[key] = maxNum);
+	}
+
+	return recurse(0, 0);
+};
