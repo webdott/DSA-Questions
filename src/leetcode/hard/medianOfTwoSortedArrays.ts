acc24d8a06b22f3497f66a8a07e5163ee8a767dc
caspero-62 <nwaforuchechukwu2007@gmail.com>
2023-09-21 14:48:59 +0100

    more solutions to problems

--- src/leetcode/hard/medianOfTwoSortedArrays.ts
+++ src/leetcode/hard/medianOfTwoSortedArrays.ts
@@ -23,7 +23,16 @@
     -106 <= nums1[i], nums2[i] <= 106
 */
 
-const findMedianSortedArrays = (nums1: number[], nums2: number[]): number => {
+/**
+ *
+ * @param nums1 number[]
+ * @param nums2 number[]
+ * @returns number
+ */
+const findMedianSortedArraysMerge = (
+	nums1: number[],
+	nums2: number[]
+): number => {
 	const mergedArray = [...nums1, ...nums2];
 	mergedArray.sort((a, b) => a - b);
 
@@ -36,4 +45,50 @@ const findMedianSortedArrays = (nums1: number[], nums2: number[]): number => {
 	} else {
 		return mergedArray[Math.floor(mergedArray.length / 2)];
 	}
+};
+
+/**
+ *
+ * @param nums1 number[]
+ * @param nums2 number[]
+ * @returns number
+ */
+const findMedianSortedArrays = (nums1: number[], nums2: number[]): number => {
+	if (nums1.length > nums2.length) {
+		[nums1, nums2] = [nums2, nums1];
+	}
+
+	const m: number = nums1.length;
+	const n: number = nums2.length;
+
+	let left: number = 0;
+	let right: number = m;
+	const mid: number = ~~((m + n + 1) / 2);
+
+	while (left <= right) {
+		let partition1: number = left + ~~((right - left) / 2);
+		let partition2: number = mid - partition1;
+
+		let maxLeft1: number = partition1 === 0 ? -Infinity : nums1[partition1 - 1];
+		let minRight1: number = partition1 === m ? Infinity : nums1[partition1];
+
+		let maxLeft2: number = partition2 === 0 ? -Infinity : nums2[partition2 - 1];
+		let minRight2: number = partition2 === n ? Infinity : nums2[partition2];
+
+		if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+			if ((m + n) % 2 === 0) {
+				return (
+					(Math.max(maxLeft1, maxLeft2) + Math.min(minRight1, minRight2)) / 2
+				);
+			} else {
+				return Math.max(maxLeft1, maxLeft2);
+			}
+		} else if (maxLeft1 > minRight2) {
+			right = partition1 - 1;
+		} else {
+			left = partition1 + 1;
+		}
+	}
+
+	return 0.0;
 };
