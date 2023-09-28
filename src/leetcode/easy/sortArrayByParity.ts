404a3586ade0823d92b096d24851dcc7955063bc
caspero-62 <nwaforuchechukwu2007@gmail.com>
2023-09-28 01:57:42 +0100

    more solutions to problems

---
+++ src/leetcode/easy/sortArrayByParity.ts
@@ -0,0 +1,43 @@
+/**
+    * desc: Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+    Return any array that satisfies this condition.
+
+    Example 1: =>
+    Input: nums = [3,1,2,4]
+    Output: [2,4,3,1]
+    Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+    
+    Example 2: =>
+    Input: nums = [0]
+    Output: [0]
+
+    Constraints: =>
+    1 <= nums.length <= 5000
+    0 <= nums[i] <= 5000
+ */
+
+/**
+ *
+ * @param nums number[]
+ * @returns number[]
+ */
+
+const sortArrayByParity = (nums: number[]): number[] => {
+	let lastEven: number = -1;
+	let lastOdd: number = 0;
+
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] % 2 === 0) {
+			lastEven = i;
+
+			while (nums[lastOdd] % 2 === 0 && lastOdd < lastEven) {
+				lastOdd += 1;
+			}
+
+			[nums[lastEven], nums[lastOdd]] = [nums[lastOdd], nums[lastEven]];
+		}
+	}
+
+	return nums;
+};
