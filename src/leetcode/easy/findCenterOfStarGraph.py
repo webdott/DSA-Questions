47d14df47e34ba572d5006cf5d4981fd324abac4
caspero-62 <nwaforuchechukwu2007@gmail.com>
2024-06-27 04:58:43 +0100

    more solutions to problems

---
+++ src/leetcode/easy/findCenterOfStarGraph.py
@@ -0,0 +1,40 @@
+# There is an undirected star graph consisting of n nodes labeled from 1 to n. A star graph is a graph where there is one center node and exactly n - 1 edges that connect the center node with every other node.
+
+# You are given a 2D integer array edges where each edges[i] = [ui, vi] indicates that there is an edge between the nodes ui and vi. Return the center of the given star graph.
+
+# Example 1: =>
+# Input: edges = [[1,2],[2,3],[4,2]]
+# Output: 2
+# Explanation: As shown in the figure above, node 2 is connected to every other node, so 2 is the center.
+
+# Example 2: =>
+# Input: edges = [[1,2],[5,1],[1,3],[1,4]]
+# Output: 1
+
+# Constraints: =>
+# 3 <= n <= 105
+# edges.length == n - 1
+# edges[i].length == 2
+# 1 <= ui, vi <= n
+# ui != vi
+# The given edges represent a valid star graph.
+
+
+from collections import defaultdict
+
+
+class Solution:
+    def findCenter(self, edges: list[list[int]]) -> int:
+        node_mapping = defaultdict(int)
+
+        for connection in edges:
+            [n1, n2] = connection
+
+            node_mapping[n1] += 1
+            node_mapping[n2] += 1
+
+        for key, value in node_mapping.items():
+            if value == len(edges):
+                return key
+
+        return -1
